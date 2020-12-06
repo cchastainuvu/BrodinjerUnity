3e77244a9c24cccb635208c0f951a97af52c6f58
Unknown <skyesladeut@outlook.com>
2020-12-06 15:59:09 -0700

    Adding Finished Geo to Whitebox Scene

--- Brodinjer/Assets/Scripts/Characters/Enemy/Attack/Enemy_Attack_Melee_Taunt.cs
+++ Brodinjer/Assets/Scripts/Characters/Enemy/Attack/Enemy_Attack_Melee_Taunt.cs
@@ -3,31 +3,45 @@ using System.Collections.Generic;
 using UnityEngine;
 public class Enemy_Attack_Melee_Taunt : Enemy_Attack_Base
 {
-    public float percentageTaunt;
+    public List<float> attackPercentages;
+    public List<bool> tauntBools;
     private float randomNum;
     public string animationFloat;
+    public List<float> attackActiveTimes;
+    private int currentnum;
     
     public override IEnumerator Attack()
     {
         GetRandom();
         animations.StartAnimation();
         yield return new WaitForSeconds(AttackStartTime);
-        if(randomNum > percentageTaunt)
-            WeaponAttackobj.SetActive(true);
-        yield return new WaitForSeconds(AttackActiveTime);
+        currentnum = GetRandom();
+        yield return new WaitForSeconds(attackActiveTimes[currentnum]);
         WeaponAttackobj.SetActive(false);
         animations.StopAnimation();
         yield return new WaitForSeconds(CoolDownTime);
 
     }
 
-    private void GetRandom()
+    private int GetRandom()
     {
         randomNum = Random.Range(0.0f, 1.0f);
-        if(randomNum <= percentageTaunt)
-            animator.SetFloat(animationFloat, 1);
-        else
-            animator.SetFloat(animationFloat, 0);
+        for (int i = 0; i < attackPercentages.Count-1; i++)
+        {
+            if (randomNum >= (attackPercentages[i]) && randomNum < attackPercentages[i + 1])
+            {
+                if (!tauntBools[i])
+                {
+                    WeaponAttackobj.SetActive(true);
+                }
+                animator.SetFloat(animationFloat, (i/(attackPercentages.Count-1)));
+                return i;
+            }
+        }
+        if(!tauntBools[tauntBools.Count-1])
+            WeaponAttackobj.SetActive(true);
+        animator.SetFloat(animationFloat, 1);
+        return attackPercentages.Count-1;
 
     }
 }
